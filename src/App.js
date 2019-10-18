df4ad42b2c3ab25e891fba0419746dc951ff20c0
LUKADk86 <lounis.86@gmail.com>
2019-10-18 22:14:02 +0200

    update item with ref= {(v)=>{this.input=v} and fucntions

--- src/App.js
+++ src/App.js
@@ -4,57 +4,65 @@ import CoursList from './components/coursList';
 import './App.css';
 
 
-class App extends React.Component{
-  state= {
+class App extends React.Component {
+  state = {
     cours: [
-      {nom: 'html'},
-      {nom: 'javascript'},
-      {nom : 'css'}
+      { nom: 'html' },
+      { nom: 'javascript' },
+      { nom: 'css' }
     ],
     current: ''
   }
-//recuperer le cours
-updateCourse = (e)=>{
-  this.setState({
-    current:e.target.value
-  })
-}
-addCourse = (e)=>{
+  //recuperer le cours
+  updateCourse = (e) => {
+    this.setState({
+      current: e.target.value
+    })
+  }
+  addCourse = (e) => {
+
+    e.preventDefault()
+    let cours = this.state.cours
+    let current = this.state.current
+    cours.push({ nom: current });
+    this.setState({
+      cours: cours,
+      current: ''
+    })
+  }
+  deleteCourse = (index) => {
+    let cours = this.state.cours;
+    cours.splice(index, 1);
+    this.setState({
+      cours: cours
+    })
+  }
+  editCours= (index, value)=>{
+    let {cours}=this.state;
+    let cour=cours[index]
+    cour['nom']=value;
+    this.setState({
+      cours:cours
+    })
+
+  }
   
-  e.preventDefault()
-  let cours = this.state.cours
-  let current = this.state.current
-  cours.push({nom: current});
-  this.setState({
-    cours: cours,
-    current: ''
-  })
-}
-deleteCourse = (index)=>{
-  let cours=this.state.cours;
-  cours.splice(index,1);
-  this.setState({
-    cours: cours
-  })
-
-}
-
 
   render() {
-  //créer une variable cours qui prend le state cours
-  const {cours} = this.state;
-  const coursList = cours.map((cour, index)=>{
-    //on passe le state.nom sous un props pour l'afficher en props dans le fichier courslist
-      return <CoursList details={cour} key={index} index={index} deleteCourse={this.deleteCourse}/>
-  }       
-  )
-  return (
-    <section className="App">
-      <h1>Ajouter cours</h1>
-      <FormCours updateCourse={this.updateCourse} addCourse={this.addCourse} current={this.state.current}/>
-      <ul>{coursList}</ul>
-    </section>
-  );
+    //créer une variable cours qui prend le state cours
+    const { cours } = this.state;
+    const coursList = cours.map((cour, index) => {
+      //on passe le state.nom sous un props pour l'afficher en props dans le fichier courslist
+      return <CoursList details={cour} key={index} index={index} deleteCourse={this.deleteCourse} editCours={this.editCours}/>
+    }
+    )
+    return (
+      <section className="App">
+        <h1>Ajouter cours</h1>
+        <FormCours updateCourse={this.updateCourse} addCourse={this.addCourse} current={this.state.current} />
+        <ul>{coursList}</ul>
+      </section>
+    );
   }
 }
 
--- src/components/coursList.js
+++ src/components/coursList.js
@@ -1,16 +1,51 @@
 import React from 'react';
 
 class CoursList extends React.Component{
-  render()
-//on utilise le fragment pour eviter la repetition de div et on recuperer la table detals envoyé par App state
-  {  return (
-    <React.Fragment>   
-            <li key={this.props.details.index}>
+  //state pour controler l affichage
+  state={
+    isEdit: false
+  }
+  renderlist=()=>{
+    return(
+      <li key={this.props.details.index}>
             {this.props.details.nom}
+            {/* on ajoute un bouton pout editer les cours onclick*/}
+            <button onClick={()=>this.toggleState()}>Edit</button>
             <button onClick={()=>this.props.deleteCourse(this.props.index)}>Delete</button>
             </li>
+    )
+  }
+  toggleState=()=>{
+    let {isEdit}=this.state;
+    this.setState({
+      isEdit: !isEdit
+    })
+   
+  }
+  submitupdatename=(e)=>{
+    e.preventDefault();
+  
+    this.props.editCours(this.props.index, this.input.value);
+    this.toggleState();
+  }
+  //state isedit appelle cette fonction en true
+  updateformCours=()=>{
+    return(
+      <form onSubmit={this.submitupdatename} >
+        <input type="text" defaultValue={this.props.details.nom} ref = {(v)=>{this.input=v}}/>
+        <button>update</button>
+      </form>
+    )
+  }
+  render()
+//on utilise le fragment pour eviter la repetition de div et on recuperer la table detals envoyé par App state
+  { 
+    let {isEdit}=this.state;
+    return (
+    <React.Fragment>   
+            
            
-              
+             {isEdit?this.updateformCours(): this.renderlist()} 
             
     </React.Fragment>
   );}
